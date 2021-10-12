7b1938f927b50352ed63be8729e67f3be26fcccc
mdafsha9 <mdafsha9@gmail.com>
2021-10-12 02:15:55 -0500

    created and added code to contact form

--- php/contact.php
+++ php/contact.php
@@ -3,14 +3,18 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "youremail@mail.com";
+
+$to = "mdafsha9@mail.com";
+
 $subject = "Mail From website";
+
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+
 $headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:thankyou.html");
+header("Location:tnk.html");
 ?>
